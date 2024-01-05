eec47448ed88bb254a595e3a31d9b7633a7c878f
Clement <paluszek.clement@gmail.com>
2024-01-05 11:44:42 +0100

    setup bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::{prelude::*, render::camera::ScalingMode, window::PresentMode};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Bevy Falling Sand".into(),
+                        resolution: (1280., 720.).into(),
+                        present_mode: PresentMode::AutoVsync,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .insert_resource(Msaa::Off)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, bevy::window::close_on_esc)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            scaling_mode: ScalingMode::Fixed { width: 1920.0, height: 1080.0 },
+            near: -1000.0,
+            ..default()
+        },
+        ..default()
+    });
 }
